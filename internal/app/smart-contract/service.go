a5d6cb80f849ed6598f657ab17bfebcc08d9dd75
pafev <paulofvilarimda@gmail.com>
2025-07-28 18:16:55 -0300

    feat(smart-contract): service get value method

--- internal/app/smart-contract/service.go
+++ internal/app/smart-contract/service.go
@@ -1,9 +1,23 @@
 package smartContractApp
 
-type SmartContractService struct{}
+import smartContractDomain "goledger-challenge-besu/internal/domain/smart-contract"
+
+type SmartContractService struct {
+	// aqui eh possivel consumir outros repositories ou executar funcoes de cache por cima
+	repository *smartContractDomain.SmartContractRepository
+}
+
+func NewService(repository *smartContractDomain.SmartContractRepository) *SmartContractService {
+	return &SmartContractService{repository}
+}
 
 func (r *SmartContractService) GetValue() ([]any, error) {
-	return []any{0}, nil
+	// para multiplas requisicoes, poderia ser implementado um sistema de cache
+	value, err := r.repository.GetValue()
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
 }
 func (r *SmartContractService) SetValue() error {
 	return nil
