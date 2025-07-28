e78a3562e20940a5792c192e6028548f9cf91d1b
pafev <paulofvilarimda@gmail.com>
2025-07-28 20:26:41 -0300

    feat(smart-contract): default uint256 in domain model for better storaging in psql

--- internal/domain/smart-contract/model.go
+++ internal/domain/smart-contract/model.go
@@ -2,12 +2,14 @@ package smartContractDomain
 
 import (
 	"time"
+
+	"github.com/core-coin/uint256"
 )
 
 type SmartContractDB struct {
 	SmartContractId uint64
 	Address         string
-	Value           [4]uint64
+	Value           uint256.Int
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 }
